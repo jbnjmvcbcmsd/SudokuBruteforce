c6d187d544cdc1c94e0a706df789945e561f9f7e
Dawid Siwek <nanoDawid@wp.pl>
2025-05-26 02:13:20 +0200

    Added comments and input validation

--- Program.cs
+++ Program.cs
@@ -8,29 +8,30 @@ namespace SudokuBruteforce
         {
             var sudokusolver = new SudokuSolver(SetSudoku());
             sudokusolver.Solve();
-            Console.WriteLine();
+            Console.WriteLine(sudokusolver.ToString());
         }
+        //Set your sudoku grid here (0 represents empty square)
         static int[,] SetSudoku()
         {
             int[,] sudoku =
             {
-                { 7, 2, 0, 0, 6, 9, 0, 0, 4
+                { 0, 0, 0, 0, 0, 0, 0, 2, 0
                 },
-                { 0, 0, 9, 0, 3, 0, 0, 5, 0
+                { 0, 0, 0, 0, 0, 0, 0, 0, 5
                 },
-                { 8, 0, 0, 4, 0, 0, 2, 6, 0
+                { 0, 2, 0, 0, 0, 0, 0, 0, 0
                 },
-                { 0, 6, 0, 9, 0, 0, 0, 0, 8
+                { 0, 0, 1, 0, 0, 0, 0, 0, 0
                 },
-                { 0, 3, 0, 0, 8, 0, 0, 1, 0
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0
                 },
-                { 5, 0, 0, 0, 0, 2, 0, 4, 0
+                { 0, 0, 0, 2, 0, 0, 0, 0, 0
                 },
-                { 0, 5, 1, 0, 0, 4, 0, 0, 6
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0
                 },
-                { 0, 8, 0, 0, 2, 0, 4, 0, 0
+                { 0, 0, 0, 0, 0, 2, 0, 0, 0
                 },
-                { 4, 0, 0, 3, 7, 0, 0, 2, 5
+                { 0, 0, 0, 0, 0, 0, 5, 0, 0
                 }
             };
             return sudoku;
--- Sudoku.cs
+++ Sudoku.cs
@@ -8,16 +8,40 @@ namespace SudokuBruteforce
 {
     public class SudokuSolver
     {
-        public int[,] Sudokutab { get; set; }
-        public int[,] Occupied { get; }
-        public List<Answer> Answers { get; set; }
+        int[,] Sudokutab { get; set; }
+        int[,] Occupied { get; }
+        List<Answer> Answers { get; set; }
+        bool Validation { get; set; } = true;
+        
         public SudokuSolver(int[,] sudoku)
         {
             Sudokutab = sudoku;
             Occupied = BuildOccupied(Sudokutab);
             Answers = new List<Answer>();
         }
-        public bool IsRowValid(int row)
+        //Checks if numbers are 0-9
+        void IsInputValid(int[,] input)
+        {
+            if(input.GetLength(0) != 9 || input.GetLength(1) != 9)
+            {
+                Validation = false; 
+                return;
+            }
+            for(int i = 0; i < input.GetLength(0); i++)
+            {
+                for(int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i,j] < 0 || input[i,j] > 9)
+                    {
+                        Validation = false;
+                        return;
+                    }
+                }
+            }
+            Validation = true;
+        }
+        //Checks if there is no duplicates in row
+        bool IsRowValid(int row)
         {
             var nums = new HashSet<int>();
             for (int i = 0; i < Sudokutab.GetLength(1); i++)
@@ -36,7 +60,8 @@ namespace SudokuBruteforce
             }
             return true;
         }
-        public bool IsColumnValid(int column)
+        //Checks if there is no duplicates in collumn
+        bool IsCollumnValid(int column)
         {
             var nums = new HashSet<int>();
             for (int i = 0; i < Sudokutab.GetLength(0); i++)
@@ -55,13 +80,15 @@ namespace SudokuBruteforce
             }
             return true;
         }
-        public int WichCell(int row, int collumn)
+        //Finds number of 1 of 9 cells
+        int WhichCell(int row, int collumn)
         {
             int x = collumn/3 + 1;
             int y = row/3;
             return x + 3 * y;
         }
-        public bool IsCellValid(int cell)
+        //Checks if there is no duplicates in cell
+        bool IsCellValid(int cell)
         {
             cell -= 1;
             int centerx = 3*(cell / 3) + 1;
@@ -86,6 +113,7 @@ namespace SudokuBruteforce
             }
             return true;
         }
+        //Checks your input and builds grid to determine which number Solve() can ignore
         int[,] BuildOccupied(int[,] tab)
         {
             int[,] occupied = new int[tab.GetLength(1),tab.GetLength(0)];
@@ -102,12 +130,14 @@ namespace SudokuBruteforce
             }
             return occupied;
         }
+        //Solves your sudoku
         public void Solve()
         {
+            IsInputValid(Sudokutab);
+            if (!Validation) { return; }
             int i = 0;
             int j = 0;
             int k = 1;
-            bool issudokuvalid = true;
             previous:
             while (i < Sudokutab.GetLength(0))
             {
@@ -118,11 +148,7 @@ namespace SudokuBruteforce
                         if(Occupied[i, j] == 0)
                         {
                             Sudokutab[i, j] = k;
-                           //Console.Clear();
-                           //ToString(Sudokutab);
-                           //Answers.ForEach(x => Console.Write(x.Value));
-                           //Thread.Sleep(50);
-                            if (IsCellValid(WichCell(i, j)) && IsColumnValid(j) && IsRowValid(i))
+                            if (IsCellValid(WhichCell(i, j)) && IsCollumnValid(j) && IsRowValid(i))
                             {
                                 Answers.Add(new Answer(j, i, k));
                                 break;
@@ -140,8 +166,8 @@ namespace SudokuBruteforce
                                 }
                                 catch(Exception x)
                                 {
-                                    issudokuvalid = false;
-                                    goto printanswer;
+                                    Validation = false;
+                                    return;
                                 }
                                 if(k > 9)
                                 {
@@ -163,21 +189,15 @@ namespace SudokuBruteforce
                 i++;
                 j = 0;
             }
-            printanswer:
-            if (issudokuvalid)
-            {
-                Console.Clear();
-                ToString(Sudokutab);
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Brak rozwiązań");
-            }
-            
         }
-        public void ToString(int[,] tab)
+        //Returns solved sudoku if exists
+        public override string ToString()
         {
+            if (!Validation)
+            {
+                return "No Answers";
+            }
+            int[,] tab = Sudokutab;
             string output = "";
             for(int i = 0; i < tab.GetLength(0); i++)
             {
@@ -195,7 +215,7 @@ namespace SudokuBruteforce
                 }
                 output += "\n";
             }
-            Console.Write(output);
+            return output;
         }
     }
 }
